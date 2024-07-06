700058c6fde7dd3fd4c4e0f472aded47fb518079
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-07-06 15:27:36 +0800

    Add a form group wrapper and labels

--- partials/input-select.php
+++ partials/input-select.php
@@ -1,8 +1,11 @@
-<select class="<?= $args['class'] ?>" name="<?= $args['name'] ?>" id="<?= $args['id'] ?>">
-  <?php if ($args['placeholder']): ?>
-    <option selected disabled value=""><?= $args['placeholder'] ?></option>
-  <?php endif; ?>
-  <?php foreach ($args['options'] as $option): ?>
-    <option value="<?= $option['value'] ?>"><?= $option['label'] ?> &mdash; <?= wc_price($option['price']) ?></option>
-  <?php endforeach; ?>
-</select>
+<div class="awv-form-group">
+  <label for="<?= $args['id'] ?>"><?= $args['label'] ?></label>
+  <select class="<?= $args['class'] ?>" name="<?= $args['name'] ?>" id="<?= $args['id'] ?>">
+    <?php if ($args['placeholder']): ?>
+      <option selected disabled value=""><?= $args['placeholder'] ?></option>
+    <?php endif; ?>
+    <?php foreach ($args['options'] as $option): ?>
+      <option value="<?= $option['value'] ?>"><?= $option['label'] ?> &mdash; <?= wc_price($option['price']) ?></option>
+    <?php endforeach; ?>
+  </select>
+</div>
--- partials/input-text.php
+++ partials/input-text.php
@@ -1,1 +1,4 @@
-<input placeholder="<?= $args['placeholder'] ?>" name="<?= $args['name'] ?>" type="<?= $args['type'] ?>">
+<div class="awv-form-group">
+  <label for="<?= $args['id'] ?>"><?= $args['label'] ?></label>
+  <input id="<?= $args['id'] ?>" name="<?= $args['name'] ?>" placeholder="<?= $args['placeholder'] ?>" type="<?= $args['type'] ?>">
+</div>
