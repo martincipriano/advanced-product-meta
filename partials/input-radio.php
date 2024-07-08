529b26fd405acb404ac4b25d445aa87403ecf5aa
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-07-08 22:34:39 +0800

    Create a partial for radio input

---
+++ partials/input-radio.php
@@ -0,0 +1,11 @@
+<div class="awv-form-group">
+  <label><?= $args['label'] ?></label>
+  <div class="apm-radio-group">
+    <?php foreach ($args['options'] as $option): ?>
+      <div class="apm-radio">
+        <input type="radio" name="<?= $args['name'] ?>" id="<?= $args['id'] . '-' . $option['value'] ?>" value="<?= $option['value'] ?>">
+        <label for="<?= $args['id'] . '-' . $option['value'] ?>"><?= $option['label'] ?> &mdash; <?= wc_price(apm_get_price($args['price'], $option['value'])) ?></label>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
