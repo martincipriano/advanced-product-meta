89008630a44c043c83ebdd3bb3ff7489627280bd
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-07-12 23:00:57 +0800

    Create a partial for the checkbox input

---
+++ partials/input-checkbox.php
@@ -0,0 +1,23 @@
+<div class="apm-form-group">
+
+  <?php if($args['label']): ?>
+    <label><?= $args['label'] ?></label>
+  <?php endif; ?>
+
+  <?php if($args['description']): ?>
+    <p class="description"><?= $args['description'] ?></p>
+  <?php endif; ?>
+
+  <?php do_action('before_apm_input', $args) ?>
+
+  <div class="apm-radio-group">
+    <?php foreach ($args['options'] as $option): ?>
+      <div class="apm-radio">
+        <input type="checkbox" name="<?= $args['name'] ?>" id="<?= $args['id'] . '-' . $option['value'] ?>" value="<?= $option['value'] ?>">
+        <label for="<?= $args['id'] . '-' . $option['value'] ?>"><?= $option['label'] ?> &mdash; <?= wc_price(apm_get_price($args['price'], $option['value'])) ?></label>
+      </div>
+    <?php endforeach; ?>
+  </div>
+
+  <?php do_action('after_apm_input', $args) ?>
+</div>
