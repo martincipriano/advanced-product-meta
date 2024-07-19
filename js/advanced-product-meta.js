0cae848a0b6b6b1d9d309bbc9e1bb47d3b214391
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-07-19 23:15:54 +0800

    Update the position of the range input value

--- js/advanced-product-meta.js
+++ js/advanced-product-meta.js
@@ -1,1 +1,28 @@
-console.log('test')
+window.apm = {}
+
+window.apm.updateRangeValue = function(e) {
+  let rangeInput = e.target
+  let formGroup = rangeInput.closest('.apm-form-group')
+  let rangeLabel = formGroup.querySelector('.apm-range-value span')
+  let max = rangeInput.getAttribute('max')
+  let min = rangeInput.getAttribute('min')
+
+  rangeLabel.style.left = `${(((rangeInput.value - min) / (max - min)) * 100)}%`
+
+  // Where 2 is the width of the range thumb in rem
+  rangeLabel.style.transform = `translateX(-${(2 * (rangeInput.value / max))}rem)`
+
+  // Update the range label with the input value
+  rangeLabel.innerHTML = rangeInput.value
+}
+
+window.apm.eventListeners = function() {
+  let ranges = document.querySelectorAll('input[type="range"]')
+
+  ranges.forEach(range => {
+    range.addEventListener('input', window.apm.updateRangeValue)
+  })
+}
+
+// run the event listeners after the dom has been loaded
+document.addEventListener('DOMContentLoaded', window.apm.eventListeners)
