0b3e4efa25f8fc171db62699e9ee2e3763422438
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-07-20 17:09:00 +0800

    Add a script to be able to interact with the range tooltip. Add styles to the range track

--- js/advanced-product-meta.js
+++ js/advanced-product-meta.js
@@ -1,26 +1,70 @@
 window.apm = {}
 
-window.apm.updateRangeValue = function(e) {
+window.apm.updateRangeTooltip = function(e) {
   let rangeInput = e.target
   let formGroup = rangeInput.closest('.apm-form-group')
-  let rangeLabel = formGroup.querySelector('.apm-range-value span')
+  let rangeTooltip = formGroup.querySelector('.apm-range-tooltip')
+  let rangeTrail = formGroup.querySelector('.apm-range-trail')
   let max = rangeInput.getAttribute('max')
   let min = rangeInput.getAttribute('min')
 
-  rangeLabel.style.left = `${(((rangeInput.value - min) / (max - min)) * 100)}%`
+  // Update the range label's left position
+  rangeTooltip.style.left = `${(((rangeInput.value - min) / (max - min)) * 100)}%`
 
-  // Where 2 is the width of the range thumb in rem
-  rangeLabel.style.transform = `translateX(-${(2 * (rangeInput.value / max))}rem)`
+  // Subtract range thumb offset where 2 is its width in rem
+  // Prevent the range trail from overlapping the range thumb
+  rangeTrail.style.width = `calc(${(((rangeInput.value - min) / (max - min)) * 100)}% - ${(2 * (rangeInput.value / max))}rem)`
+
+  // Subtract range thumb offset where 2 is its width in rem
+  // Prevent the tooltip from moving away from the center of the range thumb
+  rangeTooltip.style.transform = `translateX(-${(2 * (rangeInput.value / max))}rem)`
 
   // Update the range label with the input value
-  rangeLabel.innerHTML = rangeInput.value
+  rangeTooltip.innerHTML = rangeInput.value
+}
+
+window.apm.moveRangeTooltip = function(e) {
+  let formGroup = e.target.closest('.apm-form-group')
+  let rangeInput = formGroup.querySelector('input[type="range"]')
+  let rangeTooltip = formGroup.querySelector('.apm-range-tooltip')
+  let max = rangeInput.getAttribute('max')
+  let min = rangeInput.getAttribute('min')
+
+  function move(e) {
+    let rangeThumb = rangeInput.getBoundingClientRect()
+
+    // Assign a value to the range input based on the range label position
+    rangeInput.value = Math.round(((e.clientX - rangeThumb.left) / rangeThumb.width) * (max - min)) 
+
+    // Update the range label's left position
+    rangeTooltip.style.left = `${(((rangeInput.value - min) / (max - min)) * 100)}%`
+
+    // Subtract range thumb offset where 2 is its width in rem
+    rangeTooltip.style.transform = `translateX(-${(2 * (rangeInput.value / max))}rem)`
+
+    // Update the range label with the input value
+    rangeTooltip.innerHTML = rangeInput.value
+  }
+
+  function stop() {
+    document.removeEventListener('mousemove', move)
+    document.removeEventListener('mouseup', stop)
+  }
+
+  document.addEventListener('mousemove', move)
+  document.addEventListener('mouseup', stop)
 }
 
 window.apm.eventListeners = function() {
   let ranges = document.querySelectorAll('input[type="range"]')
+  let rangeTooltip = document.querySelectorAll('.apm-range-tooltip')
+
+  rangeTooltip.forEach(value => {
+    value.addEventListener('mousedown', window.apm.moveRangeTooltip)
+  })
 
   ranges.forEach(range => {
-    range.addEventListener('input', window.apm.updateRangeValue)
+    range.addEventListener('input', window.apm.updateRangeTooltip)
   })
 }
 
