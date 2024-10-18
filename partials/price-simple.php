49f9d144448421c7dc34a7e989e045941086d918
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-18 19:27:47 +0800

    Use the regular price if the sale price is false and add it to the subtotal

--- partials/price-simple.php
+++ partials/price-simple.php
@@ -6,19 +6,21 @@
   $regular_price = get_post_meta($product->get_id(), '_regular_price', true);
   $regular_price = $regular_price ? (float) $regular_price : 0;
 
+  $price = get_post_meta($product->get_id(), '_price', true);
+
   // Get the initial price set for the advanced product meta
   $apm_subtotal = apm_get_subtotal();
 ?>
 
 <div class="apm-price">
-  <p>
-    <strong>Initial price:</strong>
+  <p class="apm-initial-price">
+    <strong>Initial Price:</strong>
     <?php if ($sale_price): ?>
       <del><?= wc_price($regular_price) ?></del> <ins><?= wc_price($sale_price) ?></ins>
     <?php else: ?>
-      <?= wc_price($regular_price) ?>
+      <?= wc_price($price) ?>
     <?php endif; ?>
   </p>
-  <p><strong>Additional price based on selection:</strong> <span class="apm-price"><?= wc_price($apm_subtotal) ?></span></p>
-  <p><strong>Total:</strong> <span class="apm-price"><?= wc_price($sale_price + $apm_subtotal) ?></span></p>
+  <p class="apm-selection-price"><strong>Selection Price:</strong> <span class="apm-price"><?= wc_price($apm_subtotal) ?></span></p>
+  <p class="apm-total-price"><strong>Total:</strong> <span class="apm-price"><?= wc_price($price + $apm_subtotal) ?></span></p>
 </div>
